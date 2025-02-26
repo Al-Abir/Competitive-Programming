17b39bb440a8aeb6893e3fb6d713d7a1f7b19a97
Al-Abir <sheikhabir196@gmail.com>
2025-02-26 16:15:36 +0600

    Math

---
+++ Basic_Math/Seiveof.java
@@ -0,0 +1,23 @@
+package Basic_Math;
+import java.util.*;
+public class Seiveof {
+     public  static void SieveOfEratosthenes(int n) {
+      
+          boolean prime[] = new boolean[n + 1];
+           Arrays.fill(prime, true);
+    
+            for (int p = 2; p * p <= n; p++) 
+                if (prime[p]) 
+                    for (int i = p * p; i <= n; i += p) 
+                        prime[i] = false;
+    
+            for (int i = 2; i <= n; i++) 
+                if (prime[i]) System.out.print(i + " ");
+        }
+    public static void main(String[] args) {
+        
+        int n = 30;
+         SieveOfEratosthenes(n);
+        
+    }
+}
