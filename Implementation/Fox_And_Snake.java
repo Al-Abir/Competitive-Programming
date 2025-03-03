656abe4b1003f9b199eac019676c4841470f9579
Al-Abir <sheikhabir196@gmail.com>
2025-03-03 12:42:17 +0600

    Implementation

---
+++ Implementation/Fox_And_Snake.java
@@ -0,0 +1,36 @@
+        package Implementation;
+
+        import java.util.Scanner;
+
+        public class Fox_And_Snake {
+            public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+                int m = sc.nextInt();
+
+                String a = "";
+                String b = "";
+                boolean flag = true;
+
+                for(int i =0; i<m-1; i++){
+                    a+='#';
+                    b+='.';
+                }
+                a+="#";
+
+                for(int i=0; i<n; i++){
+                    if(i%2==0) System.out.println(a);
+                    else{
+                        if(flag){
+                            System.out.println(b+"#");
+                            flag = !flag;
+                        }else{
+                            System.out.println("#"+b);
+                            flag = !flag;
+                            
+                        }
+                    }
+                }
+
+            }
+        }
---
+++ Implementation/Sum_Of_Round.java
@@ -0,0 +1,47 @@
+package Implementation;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Sum_Of_Round {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int x = 1;
+            ArrayList<Integer> ans = new ArrayList<>();
+            int n = sc.nextInt();
+
+            while (n > 0) {
+                x *= 10;
+                int p = n % x;
+                if (p != 0) {
+                    ans.add(p);
+                }
+                n -= p;
+            }
+            
+          /*   while (n > 0) {
+                int p = (n % 10) * x;
+                if (p != 0) {
+                    ans.add(p);
+                }
+                n /= 10;
+                x *= 10;
+            } */
+            System.out.println(ans.size());
+
+          
+            for (int i = 0; i < ans.size(); i++) {
+                System.out.print(ans.get(i));
+                if (i != ans.size() - 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); // Move to the next line for the next test case
+        }
+        sc.close(); // Close the scanner to avoid resource leak
+    }
+}
