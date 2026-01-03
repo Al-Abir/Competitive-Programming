24c735d62a0c21ecd0ab6e65dd74749084da754f
Al-Abir <sheikhabir196@gmail.com>
2026-01-03 13:31:06 +0600

    Rated 800

---
+++ Rated_800/B_Ashmal.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        string s = "";
+        for (int i = 0; i < n; i++) {
+            string a;
+            cin >> a;
+
+            if (s.empty()) {
+                s = a;
+            } else {
+                string front = a + s;
+                string back  = s + a;
+
+                if (front < back)
+                    s = front;
+                else
+                    s = back;
+            }
+        }
+
+        cout << s << "\n";
+    }
+    return 0;
+}
